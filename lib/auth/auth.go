a885347974fb73cb52000aad9e23b851746eb715
Yassine Bounekhla <yassine@goteleport.com>
2022-08-18 11:55:47 -0400

    add assumerolerequestid to session response

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -1941,6 +1941,10 @@ func (a *Server) ExtendWebSession(ctx context.Context, req WebSessionReq, identi
 	// Keep preserving the login time.
 	sess.SetLoginTime(prevSession.GetLoginTime())
 
+	if req.AccessRequestID != "" {
+		sess.SetAssumedRoleRequestID(req.AccessRequestID)
+	}
+
 	if err := a.upsertWebSession(ctx, req.User, sess); err != nil {
 		return nil, trace.Wrap(err)
 	}
